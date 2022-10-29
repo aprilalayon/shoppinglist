52ada4c82b2e7af47fbf7976381a955be54afec4
April Alayon <eatingaprilapple@gmail.com>
2022-10-29 14:03:36 -0700

    add gitignore and rebuild

---
+++ build/precache-manifest.0eeb2c7d79322513dbec91b2b640a9a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bbedfebd59a5501976a",
+    "url": "/shoppinglist/static/css/main.20ef584f.chunk.css"
+  },
+  {
+    "revision": "5bbedfebd59a5501976a",
+    "url": "/shoppinglist/static/js/main.c1b8225e.chunk.js"
+  },
+  {
+    "revision": "e062373472c8419bcea7",
+    "url": "/shoppinglist/static/js/runtime~main.ab46ee29.js"
+  },
+  {
+    "revision": "782431a7a9fd8d41f546",
+    "url": "/shoppinglist/static/js/2.8b2938bc.chunk.js"
+  },
+  {
+    "revision": "ec29fea80f3443dc1e3b4cdb7b387b46",
+    "url": "/shoppinglist/index.html"
+  }
+];
