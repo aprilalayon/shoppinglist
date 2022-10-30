cf83889c13a25ad2e6bd039440a9e39037cdffc9
April Alayon <eatingaprilapple@gmail.com>
2022-10-30 12:26:44 -0700

    Updates

---
+++ precache-manifest.c1b94d30d82d1048fe24d7c87606483c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b16656b69afdf290b6f6",
+    "url": "/shoppinglist/static/css/main.ee8df92c.chunk.css"
+  },
+  {
+    "revision": "b16656b69afdf290b6f6",
+    "url": "/shoppinglist/static/js/main.fc90bc0a.chunk.js"
+  },
+  {
+    "revision": "e062373472c8419bcea7",
+    "url": "/shoppinglist/static/js/runtime~main.ab46ee29.js"
+  },
+  {
+    "revision": "157f51664789baf60b64",
+    "url": "/shoppinglist/static/js/2.1279535a.chunk.js"
+  },
+  {
+    "revision": "e3b2e2dee8cd997ba6217197ebf49413",
+    "url": "/shoppinglist/static/media/paper.e3b2e2de.png"
+  },
+  {
+    "revision": "c3cfd3d630a7d31631fef4915432e0b8",
+    "url": "/shoppinglist/index.html"
+  }
+];
